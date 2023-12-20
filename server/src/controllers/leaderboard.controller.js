f32aeadc6fece82f124f2b2ec9290de2405dbf6a
Banik <chiradeep13@gmail.com>
2023-12-20 18:52:19 +0000

    [MOD]

--- server/src/controllers/leaderboard.controller.js
+++ server/src/controllers/leaderboard.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 // Get overall leaderboard based on highest score
 exports.getOverallLeaderboard = async (req, res) => {
   try {
-    const leaderboard = await User.find({}, 'username highestScoreInLang highestScore').sort({ highestScore: -1 });
+    const leaderboard = await User.find({ highestScore: { $exists: true } }, 'username highestScoreInLang highestScore').sort({ highestScore: -1 });
     res.json({ leaderboard });
   } catch (error) {
     console.error(error);
@@ -11,11 +11,14 @@ exports.getOverallLeaderboard = async (req, res) => {
   }
 };
 
+
 // Get leaderboard based on highest score and highestScoreInLang for a specific language
 exports.getLanguageLeaderboard = async (req, res) => {
   try {
     const { language } = req.params;
-    const leaderboard = await User.find({ highestScoreInLang: language }, 'username highestScore').sort({ highestScore: -1 });
+    const leaderboard = await User.find({ 
+      highestScoreInLang: language,
+      highestScore: { $exists: true } }, 'username highestScore').sort({ highestScore: -1 });
     res.json({ leaderboard });
   } catch (error) {
     console.error(error);
