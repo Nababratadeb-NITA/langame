e0c9e1aa00162e1c9e5065c8190c100f9077bb0f
Nababratadeb-NITA <nababrata13@gmail.com>
2023-12-22 03:30:44 +0530

    create game logic and basic of design [WIP]

--- langame/app/components/Ques.jsx
+++ langame/app/components/Ques.jsx
@@ -1,65 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Ques({ question, options, onOptionSelect }) {
-    return (
-        <div class="max-w-2xl mx-auto text-black bg-white p-8 rounded-md">
-            <div id="default-carousel" class="relative" data-carousel="static">
-                <div class="overflow-hidden relative h-[30rem] rounded-lg">
-                    <h1>
-                        <span>Q.1 </span>{question}
-                    </h1>
+function Ques({ data, questionNumber, setQuestionNumber, setTimeOut }) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
 
-                    <div class="text-black h-7">
-                        <div class="container mx-auto">
-                            <div class="mt-6 space-y-8 xl:mt-12">
-                                {options.map((option, i) => {
-                                    return (<div class="flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border border-blue-500 cursor-pointer rounded-xl">
-                                        
+  console.log(question);
 
-                                        <h2 class="text-base font-semibold text-gray-600 sm:text-xl">
-                                            {option}
-                                        </h2>
-                                    </div>)
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-                    <button
-                        type="button"
-                        class="w-3 h-3 rounded-full"
-                        aria-current="false"
-                        aria-label="Slide 1"
-                        data-carousel-slide-to="0"
-                    ></button>
-                    <button
-                        type="button"
-                        class="w-3 h-3 rounded-full"
-                        aria-current="false"
-                        aria-label="Slide 2"
-                        data-carousel-slide-to="1"
-                    ></button>
-                    <button
-                        type="button"
-                        class="w-3 h-3 rounded-full"
-                        aria-current="false"
-                        aria-label="Slide 3"
-                        data-carousel-slide-to="2"
-                    ></button>
-                </div>
-                
-            </div>
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
 
-            <button
-                class="middle none center w-full rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                data-ripple-light="true"
-            >
-                Check
-            </button>
-            <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
-        </div>
-    );
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+      delay(5000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+        delay(1000, () => {
+          setTimeOut(true);
+        });
+      }
+      })
+  };
+
+    
+
+
+  return (
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.options.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => !selectedAnswer && handleClick(a)}
+          >
+            {a}
+          </div>
+        ))}
+      </div>
+    </div>   
+  );
 }
 
 export default Ques;
--- langame/app/components/Question.jsx
+++ langame/app/components/Question.jsx
@@ -1,15 +1,15 @@
 // pages/index.js
 
-import { useState, useEffect } from 'react';
-import Ques from './Ques';
+import { useState, useEffect, useMemo } from "react";
+import Ques from "./Ques";
+import Timer from "./Timer";
 
 const QuizApp = ({ selectedLanguage, setSelectedLanguage }) => {
   const [questions, setQuestions] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(1);
   const [score, setScore] = useState(0);
-  const [userAnswer, setUserAnswer] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
   const [loading, setLoading] = useState(true);
-
 
   useEffect(() => {
     // Fetch questions based on the selected language
@@ -17,11 +17,13 @@ const QuizApp = ({ selectedLanguage, setSelectedLanguage }) => {
       try {
         setLoading(true);
         let lang = selectedLanguage.toLowerCase();
-        const response = await fetch(`https://langgame-server.onrender.com/api/question/language/${lang}`);
+        const response = await fetch(
+          `https://langgame-server.onrender.com/api/question/language/${lang}`
+        );
         const data = await response.json();
         setQuestions(data.questions);
       } catch (error) {
-        console.error('Error fetching questions:', error);
+        console.error("Error fetching questions:", error);
       } finally {
         setLoading(false);
       }
@@ -30,51 +32,77 @@ const QuizApp = ({ selectedLanguage, setSelectedLanguage }) => {
     fetchQuestions();
   }, [selectedLanguage]);
 
-  const handleCheckAnswer = () => {
-    if (userAnswer === questions[currentQuestionIndex].correctOption) {
-      setScore(score + 1);
-    }
+  const scorePyramid = useMemo(
+    () =>
+      [
+        { id: 1, score: "1" },
+        { id: 2, score: "2" },
+        { id: 3, score: "3" },
+        { id: 4, score: "5" },
+        { id: 5, score: "10" },
+        { id: 6, score: "20" },
+        { id: 7, score: "40" },
+        { id: 8, score: "80" },
+        { id: 9, score: "100" },
+        { id: 10, score: "200" },
+        { id: 11, score: "400" },
+        { id: 12, score: "1200" },
+        { id: 13, score: "2000" },
+        { id: 14, score: "5000" },
+        { id: 15, score: "10000" },
+      ].reverse(),
+    []
+  );
 
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setUserAnswer(null);
-    }
-  };
-
-  const handleOptionSelect = (selectedOption) => {
-    setUserAnswer(selectedOption);
-  };
-
-  if (loading) {
-    return <div className="quiz-container mx-auto my-auto p-8 max-w-md">Loading...</div>;
-  }
-
-  const currentQuestion = questions[currentQuestionIndex];
+  useEffect(() => {
+    questionNumber > 1 &&
+      setScore(scorePyramid.find((m) => m.id === questionNumber - 1).score);
+  }, [questionNumber, scorePyramid]);
 
   return (
     <div className="quiz-container h-screen mx-auto p-8 max-w-md">
       <h1 className="text-2xl font-bold mb-4">Quiz App</h1>
-
-      {questions.length > 0 && currentQuestion ? (
-        <Ques
-          question={currentQuestion.question}
-          options={currentQuestion.options}
-          onOptionSelect={handleOptionSelect}
-        />
+      {timeOut ? (
+        <h1 className="endText">You scored: {score}</h1>
       ) : (
-        <p>No questions available.</p>
+        <>
+          <div className="top">
+            <div className="timer">
+              <Timer
+                setTimeOut={setTimeOut}
+                questionNumber={questionNumber}
+              />
+            </div>
+          </div>
+          <div className="bottom">
+            <Ques
+              data={questions}
+              questionNumber={questionNumber}
+              setQuestionNumber={setQuestionNumber}
+              setTimeOut={setTimeOut}
+            />
+          </div>
+        </>
       )}
-      
-      {currentQuestionIndex === questions.length - 1 && (
-        <div className="text-2xl font-bold mt-4">
-          Your Final Score: {score}/{questions.length}
-        </div>
-      )}
-     
+
+      <div className="pyramid">
+      <ul className="moneyList">
+        {scorePyramid.map((m) => (
+          <li
+            className={
+              questionNumber === m.id
+                ? "moneyListItem active"
+                : "moneyListItem"
+            }
+          >
+            <span className="moneyListItemNumber">{m.id}</span>
+            <span className="moneyListItemAmount">{m.amount}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
     </div>
   );
 };
 
 export default QuizApp;
-
-
